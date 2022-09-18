33c9486daa134cbe9f4ed61c2137423a2db190bb
Lohenl <winsobaggins@yahoo.com.sg>
2022-09-18 23:14:04 +0800

    comments out second bucket

--- main.tf
+++ main.tf
@@ -24,29 +24,29 @@ resource "aws_s3_bucket" "my-test-bucket" {
 }
 
 # Basic S3 bucket with variable reference
-resource "aws_s3_bucket" "my-test-bucket-referenced" {
-  bucket = var.bucketname
-  tags = {
-    Name        = "My other bucket"
-    Environment = "Dev"
-  }
-}
+# resource "aws_s3_bucket" "my-test-bucket-referenced" {
+#   bucket = var.bucketname
+#   tags = {
+#     Name        = "My other bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 # Uploads object to S3 bucket (example)
-resource "aws_s3_bucket_object" "object" {
-  bucket = aws_s3_bucket.my-test-bucket-referenced.id
-  key    = var.s3objectkey
-  source = var.sourcepath
+# resource "aws_s3_bucket_object" "object" {
+#   bucket = aws_s3_bucket.my-test-bucket-referenced.id
+#   key    = var.s3objectkey
+#   source = var.sourcepath
 
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  # etag = filemd5("path/to/file")
-}
+#   # The filemd5() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+#   # etag = "${md5(file("path/to/file"))}"
+#   # etag = filemd5("path/to/file")
+# }
 
 
 # S3 ACL for demo bucket to allow PUT access with key
-resource "aws_s3_bucket_acl" "my-test-bucket-acl" {
-  bucket = aws_s3_bucket.my-test-bucket-referenced.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "my-test-bucket-acl" {
+#   bucket = aws_s3_bucket.my-test-bucket-referenced.id
+#   acl    = "private"
+# }
